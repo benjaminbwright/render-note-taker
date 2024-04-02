1388c5a5c92f9d874e9f9707b08aef57698d53a2
Ben Wright <bwright@2u.com>
2024-04-02 13:52:41 -0500

    Adds gitignore and removes node modules

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+// home page
+// notes page
+// GET /api/notes
+// GET /api/notes/:id
+// POST /api/notes
+// DELETE /api/notes
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
